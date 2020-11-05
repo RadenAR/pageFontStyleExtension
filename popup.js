08ed2e268ad064e396a84ca36fe2a4ae8001c682
RadenAR <radenabdulrahman@gmail.com>
2020-11-05 12:46:03 -0500

    Finished version

---
+++ popup.js
@@ -0,0 +1,14 @@
+$(function(){
+    color = $('#fontColor').val();
+    $("#fontColor").on("change paste keyup", function() {
+        color = $(this).val(); 
+    });
+    
+   $('#btnChange').click(function(){      
+         chrome.tabs.query({active:true,currentWindow: true}, function(tabs){
+            console.log(tabs)
+            chrome.tabs.sendMessage(tabs[0].id, {todo: "changeColor", clickedColor: color });
+        });
+   });
+});
+
